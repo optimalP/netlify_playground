a6861e8170571ea15b8fc9ddce6e8b1ab8f2cf5f
daybues <daybues@gmail.com>
2024-10-04 22:18:16 -0700

    playing around with a personal website

--- src/App.js
+++ src/App.js
@@ -1,19 +1,8 @@
-import React from "react";
-import './App.css';
-import Header from './components/Header';
-import Home from './components/Home';
-import About from './components/About';
-//import IdeaValidation from './components/IdeaValidation';
-import Contact from './components/Contact';
-import Footer from './components/Footer';
+import React from 'react';
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <main>
-        <Home />
-        <About />
       <h1>Netlify Form Test</h1>
 
       {/* Simple form for testing Netlify submission */}
@@ -40,9 +29,6 @@ function App() {
           <button type="submit">Submit</button>
         </p>
       </form>
-        <Contact />
-      </main>
-      <Footer />
     </div>
   );
 }
